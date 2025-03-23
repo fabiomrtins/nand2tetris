3386dddd11141428f6e9696b553d59cfbd6e8b39
Fábio Martins <fabio.martins@tascomtecnologia.com.br>
2025-03-23 19:26:04 -0300

    created project 4 folder and Mult.asm

---
+++ projects/4/Mult.asm
@@ -0,0 +1,35 @@
+@sum
+M=0
+@i
+M=0
+
+(IS_I_LESSER_THAN_R1)
+    @R1
+    D=M-D // R1 - @i
+    @LOOP
+    D;JGT // if (R1 > @i) go again
+
+    @FINISH
+    0;JMP
+
+(LOOP)
+    @R0
+    D=M
+    @sum
+    M=D+M // sum = R0 + sum
+
+    @i
+    M=M+1 // i++
+    D=M
+    @IS_I_LESSER_THAN_R1
+    0;JMP
+
+(FINISH)
+    @sum
+    D=M
+    @R2
+    M=D // R2 = sum
+
+    (END)
+    @END
+    0;JMP
